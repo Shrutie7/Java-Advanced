742999672b3d6ba4f28e02b72287c23208a59c3f
Steak <lakhotiya.sil07@gmail.com>
2026-05-05 04:16:35 +0530

    Added Atomic & Volatile

--- Collection/AtomicvsVolatile/VolatileCounter.java
+++ Collection/AtomicvsVolatile/VolatileCounter.java
@@ -1,7 +1,49 @@
 package Collection.AtomicvsVolatile;
 
+
+//VOLATILE --> JITNE BHI THREADS HAI UNKO DIRECTLY VALUE MAIN MEMORY SE UTHANI H YA WRITE KRNI H
+// EK TIME PE 2 THREADS COUNTER KO BADHA SAKTE H NON BLOCKING H WAHI DIKKAT H 20000 KABHI NHI AAIGA
+// BS THREAD KI LOCAL COPY NHI HOGI VARIABLE KI, BS SIMPLE FLAG/STATE(TRUE/FALSE) KE CASE ME USE KRNA VOLATILE KO
+
+//ATOMICITY -->An operation is done completely by one thread without interruption from other threads.
+//Java provides Atomic classes to make operations atomic.
+//HAR EK OPERATION ISOLATE RHE EK BAR ME EK HI THREAD ACCESS KRE (KYUKI 2 THREAD COUNTER KO EK SATH ACCESS KRRI)
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+//WE CAN USE SYNCHRONIZED/LOCK/ATOMIC class given by java --›(Atomic Integer/Atomic Long etc) ensure ATOMICITY/ISOLATION W/O USING LOCKS
+//ATOMIC CLASSES IN JAVA --> THREAD SAFE (ek bar me ek hi thread kam krega)
 public class VolatileCounter {
-    public static void main(String[] args) {
-        
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    private void increment() { counter.incrementAndGet();}// inbuilt method h yeh incrementAndGet()}
+    private int getCounter() {return counter.get();} // inbuilt method h yeh get()
+    public static void main(String[] args) throws InterruptedException {
+
+        VolatileCounter vc = new VolatileCounter();
+
+        //2 writer threads hai
+        Thread t1 = new Thread (()-> {
+            for (int i = 0; i < 10000; i++) {
+                vc.increment();
+            }
+        });
+
+        Thread t2 = new Thread (()-> {
+            for (int i = 0; i < 10000; i++) {
+                vc.increment();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println(vc.getCounter());  // will not give you 20000 that's issue with volatile its not BLOCKING
+
+
+
     }
 }
--- Collection/AtomicvsVolatile/VolatileExample.java
+++ Collection/AtomicvsVolatile/VolatileExample.java
@@ -1,7 +1,63 @@
 package Collection.AtomicvsVolatile;
 
-public class VolatileExample {
-    public static void main(String[] args) {
+// *****every THREAD keeps LOCAL COPY of variables SAVED IN ITS CACHE for performance reasons**
+//VOLATILE --> JITNE BHI THREADS HAI UNKO DIRECTLY VALUE MAIN MEMORY SE UTHANI H YA WRITE KRNI H --> NOT BLOCKING
+// PR VOLATILE ME 2 THREADS EK SATH ACCESS KRR RHE H VARIABLE KO BLOCKING NHI H DIKKAT H (SYNCHRONIZED KRYWORD TABHI toh AAYA)
 
+
+
+class SharedObj{
+    private volatile boolean flag=false;
+
+    //writer chalaega setFlagTrue() method
+
+    public void setFlagTrue() {
+        System.out.println("Writer thread made the flag true");
+        flag = true;
+    }
+
+    //reader chalaega PrintIfFlagTrue() method
+    public void PrintIfFlagTrue(){
+        while (!flag){
+            //do nothing
+        }
+        System.out.println("Flag is true");
+    }
+
+}
+public class VolatileExample {
+    public static void main(String[] args) throws InterruptedException {
+        //boolean flag is there initially false,2 threads are there reader thread and writer thread
+        // writer thread flag ko true krega and reader thread flag ko read krega
+
+        SharedObj sharedObj = new SharedObj();
+        Thread WriterThread = new Thread(() -> {
+            try {
+                Thread.sleep(1000);//1s wait, chahte h writerThread flag ko true krne se pehle hi reader thread chl jaye and while loop me fas jaye
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            sharedObj.setFlagTrue();
+        });
+        Thread ReaderThread = new Thread(() -> sharedObj.PrintIfFlagTrue());
+        WriterThread.start();
+        ReaderThread.start();
+
+        //output me Writer thread made the flag true aaiga print hoga and flag true bhi hoga but reader Thread ko nhi dikh rhah flag true hogya toh wo loop me hi h
+        // readerThred ko nhi dikh rhah flag true bcoz **every thread keeps local copy of variables apne cache mein save krletah for performance reasons**
+        // flag changed in main memory but in readerThread the flag is cached as FALSE WO CHANGE NHI HUA H
+        // BCOZ FLAG IS SHARED ONE PLACE WE WRITE AND ONE PLACE WE READ
+
+
+
+
+        // ********TO NOT CACHE THE VARIABLE IN A THREAD WHENEVER WE HAVE TO READ/WRITE MAIN MEMORY SE HI READ/WRITE KRIYE****************
+        // *************************************SO WRITE VOLATILE IN THE VARIABLE*********************************************************
+        // then after 1s writer thread will make flag true and reader thread ko dikh jaiga flag true hogya wo loop se bahar ajiaga bcoz flag variable hamne volatile banai h
+        // so readerthread apne local cache se data nhi leke main memory se data lera
+
+
+        // LIMITATIONS OF VOLATILE --> SYNCHRONIZE OR LOCKS BCOZ THREADS ARE NOT BLOCKING HERE
+        // USE VOLATILE-> USE AS SIMPLE FLAG WHICH TELL STATE CHANGE LIKE IN THIS EXAMPLE/ MULTIPLE THREADS KO DEKHNA H not for COMPLEX
     }
 }
