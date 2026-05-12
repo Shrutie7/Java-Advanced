0c42643d25649ff8f9f63f407ad048c013dd3aa7
Steak <lakhotiya.sil07@gmail.com>
2026-05-12 23:57:37 +0530

    Added Thread Lifecycle

--- Collection/MultiThreading/MultiThreadingTest2.java
+++ Collection/MultiThreading/MultiThreadingTest2.java
@@ -1,4 +1,27 @@
 package Collection.MultiThreading;
 
 public class MultiThreadingTest2 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+
+
+        System.out.println(Thread.currentThread().getName()); //main
+
+
+
+        // to print name of thread world (by default Thread-0) use class World class ka object banana hoga
+        World world = new World();
+        //world.start();//to start the thread when extends Thread class then use .start() method
+        Thread t1 = new Thread(world); // do this when class implements Runnable interface
+        t1.start();
+
+
+        //infinite loop chalega
+        for (;;){
+            System.out.println(Thread.currentThread().getName());
+        }
+
+    }
 }
--- Collection/MultiThreading/MyThreadLifeCycle4.java
+++ Collection/MultiThreading/MyThreadLifeCycle4.java
@@ -1,4 +1,44 @@
 package Collection.MultiThreading;
 
-public class MyThreadLifeCycle4 {
+public class MyThreadLifeCycle4 extends Thread {
+    @Override
+    public void run() {
+        System.out.println("RUNNING");
+        // sleep t1 thread also for 2s
+        // here Thread.sleep will throw error enclose in try catch block // we cant write in method signature throws Interrupted Exception bcoz run method is overridden throws error
+        try{//t1 ko pause kra diya h
+            Thread.sleep(2000);
+        }catch (InterruptedException e){
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException{
+        //getState() method returns State which is enum --> NEW, RUNNABLE, BLOCKED, WAITING, TIMED_WAITING, TERMINATED
+        // BUT IN JAVA in enum State RUNNING STATE IS NOT THERE BCOZ RUNNABLE MEANS EITHER ITS RUNNING OR READY TO RUN
+        MyThreadLifeCycle4 t1 = new MyThreadLifeCycle4();// NEW STATE
+        System.out.println(t1.getState()); //NEW
+        t1.start();
+        System.out.println(t1.getState()); // RUNNABLE STATE
+
+
+        //this all operations is done by Main Thread
+        // so to check call Thread.currentThread() in whichever method u will see that Thread
+        System.out.println(Thread.currentThread().getState());//main thread ki state bata raha h // RUNNABLE bcoz Running state not there // RUNNABLE
+
+
+
+        Thread.sleep(100);
+        System.out.println(t1.getState());
+
+
+        //. join() --> Waits for this thread to die.
+        // An invocation of this method behaves in exactly the same way as the invocation
+        t1.join(); //
+        System.out.println(t1.getState()); // jis thread pe call krre usko finish hone ka wait krra main thread chala raha isko main thread --> main method'll wait for t1 to get finished tabhi main method will continue further
+
+        //THREAD VS RUNNABLE
+        // 2 WAYS TO CREATE A THREAD -→> 1. EXTENDS THREAD CLASS 2. IMPLEMENT RUNNABLE INTERFACE
+        // WHEN TO USE WHAT?????
+    }
 }
